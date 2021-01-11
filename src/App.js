bcb14ee2c14926067ab2e6585528c21b36f8a0fa
GorilaNaranja <woodworkerdeveloper@gmail.com>
2021-01-11 12:09:36 +0100

    Props II

--- src/App.js
+++ src/App.js
@@ -10,22 +10,42 @@ const Hello = (props) => <h3>{props.title}</h3>;
 
 class Hola extends Component {
   render() {
-    return <h3>{this.props.title}</h3>;
+    return (
+      <div>
+        <h3>{this.props.title}</h3>
+        <h3>{this.props.defautlName}</h3>
+      </div>
+    );
   }
 }
 
+Hola.defaultProps = {
+  title: "My default title",
+  defautlName: "Felipe",
+};
+
 class Text extends Component {
   render() {
-    const { isActive, ages, text, number, car } = this.props;
+    const {
+      isActive,
+      ages,
+      text,
+      number,
+      car,
+      multiply,
+      description,
+    } = this.props;
     const info = isActive ? "Show info" : "Dont show info";
     const mappedAges = ages.map((age) => age * 2);
     return (
       <div>
         <p>{text}</p>
         <p>{number}</p>
+        {description}
         <p>{info}</p>
         <p>{mappedAges.join(", ")}</p>
         <p>{car.marca}</p>
+        <p>{multiply(5)}</p>
       </div>
     );
   }
@@ -39,11 +59,13 @@ function App() {
         <Hello title="Component from function" />
         <Hola title="Component from class" />
         <Text
-          car={{ marca: "Toyota", model: "Yaris" }}
           text="My favourite number: "
+          description={<p>My description</p>}
           number={22}
+          car={{ marca: "Toyota", model: "Yaris" }}
           isActive={true}
           ages={[20, 30, 40]}
+          multiply={(number) => number * 2}
         />
       </header>
     </div>
