f8a5616f5a988187c10a7a3e31bc0c4d7009e56c
Felipe <woodworkerdeveloper@gmail.com>
2021-10-22 16:35:31 +0200

    New: axios api service

---
+++ src/services/api.js
@@ -0,0 +1,39 @@
+import axios from 'axios';
+
+const API_KEY = process.env.REACT_APP_API_KEY;
+const URL = process.env.REACT_APP_URL;
+const headers = { Authorization: `Bearer ${API_KEY}` };
+const query = '?page=1&limit=10';
+
+const api = axios.create({
+  baseURL: URL,
+  headers,
+});
+
+const getCharacters = async () => {
+  const response = await api.get(`/character${query}`);
+  return response.data;
+};
+
+// const getCharacterById = async (id) => {
+//   const response = await axios.get(
+//     `${URL}/character/${id}?page=1&limit=10`,
+//     headers
+//   );
+//   return response.data;
+// };
+
+// const getBooks = async () => {
+//   const response = await axios.get(`${URL}/book?page=1&limit=10`, headers);
+//   return response.data;
+// };
+
+// const getBookById = async (id) => {
+//   const response = await axios.get(
+//     `${URL}/book/${id}?page=1&limit=10`,
+//     headers
+//   );
+//   return response.data;
+// };
+
+export { getCharacters /*getCharacterById, getBooks, getBookById*/ };
